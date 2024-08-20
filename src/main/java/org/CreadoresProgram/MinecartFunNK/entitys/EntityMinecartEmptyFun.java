5ce3ec910cef28d08f6ae293dc129f4401806571
Creadores Program <cpcreador@gmail.com>
2024-08-20 13:31:21 -0600

    Update EntityMinecartEmptyFun.java

--- src/main/java/org/CreadoresProgram/MinecartFunNK/entitys/EntityMinecartEmptyFun.java
+++ src/main/java/org/CreadoresProgram/MinecartFunNK/entitys/EntityMinecartEmptyFun.java
@@ -1,5 +1,21 @@
 package org.CreadoresProgram.MinecartFunNK.entitys;
 import cn.nukkit.entity.item.EntityMinecartEmpty;
+import cn.nukkit.Player;
+import cn.nukkit.math.MathHelper;
 public class EntityMinecartEmptyFun extends EntityMinecartEmpty{
-  
+  @Override
+  public boolean onUpdate(int currentTik){
+    if(this.closed){
+      return false;
+    }
+    boolean update = super.onUpdate(currentTik);
+    if(this.isAlive()){
+      if(this.passengers.get(0) == null || (!(this.passengers.get(0) instanceof Player) || this.passengers.size() > 1)) return false;
+      this.lastX = this.x;
+      this.lastY = this.y;
+      this.lastZ = this.z;
+      this.motionY -= 0.04;
+      
+    }
+  }
 }
