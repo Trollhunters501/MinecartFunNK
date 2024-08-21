6c79b3ca7b355d2ab07d9f3f473f079af1c95e4e
Creadores Program <cpcreador@gmail.com>
2024-08-20 20:22:40 -0600

    Update Main.java

--- src/main/java/org/CreadoresProgram/MinecartFunNK/Main.java
+++ src/main/java/org/CreadoresProgram/MinecartFunNK/Main.java
@@ -1,6 +1,14 @@
 package org.CreadoresProgram.MinecartFunNK;
 import cn.nukkit.Player;
 import cn.nukkit.plugin.PluginBase;
+import org.CreadoresProgram.MinecartFunNK.entitys.EntityMinecartEmptyFun;
 public class Main extends PluginBase{
-  
+  @Override
+  public void onEnable(){
+    this.getLogger().info("§aDone!");
+  }
+  @Override
+  public void onLoad(){
+    this.getLogger().info("§eLoading...");
+  }
 }
